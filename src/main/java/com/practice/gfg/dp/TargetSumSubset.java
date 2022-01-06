e2b070a65835be084cc5118d080320d6b93a63a7
Rahul Gupta <rahulxmartinian@gmail.com>
2022-01-06 10:19:19 +0530

    Practice.

--- src/main/java/com/practice/gfg/dp/TargetSumSubset.java
+++ src/main/java/com/practice/gfg/dp/TargetSumSubset.java
@@ -2,31 +2,40 @@ package com.practice.gfg.dp;
 
 /**
  * https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
+ *
+ * Create a 2D array of size [arr.length + 1][targetSum + 1]
  */
 public class TargetSumSubset {
     public static boolean targetSumSubset(int[] arr, int targetSum) {
         boolean isAvailable = false;
 
-        boolean[][] targetSumArr = new boolean[targetSum + 1][arr.length + 1];
+        boolean[][] targetSumArr = new boolean[arr.length + 1][targetSum + 1];
 
-        for (int i = 0; i < targetSum + 1; i++) {
-            for (int j = 0; j < arr.length + 1; j++) {
-                if (i ==0 && j == 0) {
+        for (int i = 0; i < arr.length + 1; i++) {
+            for (int j = 0; j < targetSum + 1; j++) {
+                if (i == 0 && j == 0) {
                     targetSumArr[i][j] = true;
                 } else if (i == 0) {
-                    targetSumArr[i][j] = true;
-                } else if (j == 0) {
                     targetSumArr[i][j] = false;
+                } else if (j == 0) {
+                    targetSumArr[i][j] = true;
                 } else {
-
+                    if (targetSumArr[i - 1][j])
+                        targetSumArr[i][j] = true;
+                    else {
+                        int currentVal = arr[i - 1];
+                        if (j >= currentVal)
+                            targetSumArr[i][j] = targetSumArr[i - 1][j - currentVal];
+                    }
                 }
             }
         }
 
-        return isAvailable;
+        return targetSumArr[arr.length][targetSum];
     }
 
     public static void main(String[] args) {
-        targetSumSubset(new int[]{3, 34, 4, 12, 5, 2}, 9);
+        System.out.println(targetSumSubset(new int[]{3, 34, 4, 12, 5, 2}, 9));
+        //System.out.println(targetSumSubset(new int[]{4,2, 3, 1}, 10));
     }
 }
